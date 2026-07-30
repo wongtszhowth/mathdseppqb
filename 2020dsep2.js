266dadec4690d34602b44b053319302f620fdc29
TH Wong <wongth@chusheklun.edu.hk>
2026-07-30 09:23:31 +0800

    Update 2020dsep2.js

--- 2020dsep2.js
+++ 2020dsep2.js
@@ -11,10 +11,10 @@ questions.push(
         difficulty: 1,
         content: `$\\dfrac{6x}{(3x^{-5})^{-2}} =$
 <div class="q-mcopt">
-A.\t$54x^8$<br>
-B.\t$\\dfrac{2x^8}{3}$<br>
-C.\t$\\dfrac{54}{x^9}$<br>
-D.\t$\\dfrac{2}{3x^9}$
+A.\t$54x^8$。<br>
+B.\t$\\dfrac{2x^8}{3}$。<br>
+C.\t$\\dfrac{54}{x^9}$。<br>
+D.\t$\\dfrac{2}{3x^9}$。
 </div>`
     },
     {
@@ -27,10 +27,10 @@ D.\t$\\dfrac{2}{3x^9}$
         difficulty: 1,
         content: `若 $a(a+b)=2(b-a)$，則 $b=$
 <div class="q-mcopt">
-A.\t$\\dfrac{a^2+a}{2+a}$<br>
-B.\t$\\dfrac{a^2-2a}{2+a}$<br>
-C.\t$\\dfrac{a^2+2a}{2-a}$<br>
-D.\t$\\dfrac{a^2-a}{2-a}$
+A.\t$\\dfrac{a^2+a}{2+a}$。<br>
+B.\t$\\dfrac{a^2-2a}{2+a}$。<br>
+C.\t$\\dfrac{a^2+2a}{2-a}$。<br>
+D.\t$\\dfrac{a^2-a}{2-a}$。
 </div>`
     },
     {
@@ -43,10 +43,10 @@ D.\t$\\dfrac{a^2-a}{2-a}$
         difficulty: 1,
         content: `$\\dfrac{5}{4k+3} - \\dfrac{2}{4k-3} =$
 <div class="q-mcopt">
-A.\t$\\dfrac{12k-21}{16k^2-9}$<br>
-B.\t$\\dfrac{12k+9}{16k^2-9}$<br>
-C.\t$\\dfrac{14k-21}{16k^2-9}$<br>
-D.\t$\\dfrac{14k+9}{16k^2-9}$
+A.\t$\\dfrac{12k-21}{16k^2-9}$。<br>
+B.\t$\\dfrac{12k+9}{16k^2-9}$。<br>
+C.\t$\\dfrac{14k-21}{16k^2-9}$。<br>
+D.\t$\\dfrac{14k+9}{16k^2-9}$。
 </div>`
     },
     {
@@ -59,10 +59,10 @@ D.\t$\\dfrac{14k+9}{16k^2-9}$
         difficulty: 2,
         content: `$(3a+2b)(4a-5b) - a(6a+4b) =$
 <div class="q-mcopt">
-A.\t$(3a+2b)(2a-5b)$<br>
-B.\t$(3a+2b)(6a-5b)$<br>
-C.\t$(3a-2b)(2a+5b)$<br>
-D.\t$(3a-2b)(6a+5b)$
+A.\t$(3a+2b)(2a-5b)$。<br>
+B.\t$(3a+2b)(6a-5b)$。<br>
+C.\t$(3a-2b)(2a+5b)$。<br>
+D.\t$(3a-2b)(6a+5b)$。
 </div>`
     },
     {
@@ -73,12 +73,12 @@ D.\t$(3a-2b)(6a+5b)$
         domain: ["Algebra"],
         topic: ["Functions and Graphs"],
         difficulty: 1,
-        content: `設 $\\mathrm{f}(x)=3x^2-x-2$。若 $\\beta$ 為一常數，則 $\\mathrm{f}(1+\\beta)-\\mathrm{f}(1-\\beta) =$
+        content: `設 $\\mathrm{f}(x)=3x^2-x-2$。 若 $\\beta$ 為一常數，則 $\\mathrm{f}(1+\\beta)-\\mathrm{f}(1-\\beta) =$
 <div class="q-mcopt">
-A.\t$2\\beta$<br>
-B.\t$10\\beta$<br>
-C.\t$6\\beta^2-2$<br>
-D.\t$6\\beta^2-2\\beta$
+A.\t$2\\beta$。<br>
+B.\t$10\\beta$。<br>
+C.\t$6\\beta^2-2$。<br>
+D.\t$6\\beta^2-2\\beta$。
 </div>`
     },
     {
@@ -89,12 +89,12 @@ D.\t$6\\beta^2-2\\beta$
         domain: ["Algebra"],
         topic: ["Polynomials"],
         difficulty: 2,
-        content: `設 $\\mathrm{g}(x)=ax^3+4ax^2-24$，其中 $a$ 為一常數。若 $x+2$ 為 $\\mathrm{g}(x)$ 的因式，則 $\\mathrm{g}(2) =$
+        content: `設 $\\mathrm{g}(x)=ax^3+4ax^2-24$，其中 $a$ 為一常數。 若 $x+2$ 為 $\\mathrm{g}(x)$ 的因式，則 $\\mathrm{g}(2) =$
 <div class="q-mcopt">
-A.\t$-96$<br>
-B.\t$0$<br>
-C.\t$3$<br>
-D.\t$48$
+A.\t$-96$。<br>
+B.\t$0$。<br>
+C.\t$3$。<br>
+D.\t$48$。
 </div>`
     },
     {
@@ -105,12 +105,12 @@ D.\t$48$
         domain: ["Algebra"],
         topic: ["Polynomials"],
         difficulty: 2,
-        content: `若 $h$ 及 $k$ 均為常數使得 $(x+h)(x+6) = (x+4)^2+k$，則 $k =$
+        content: `若 $h$ 及 $k$ 均為常數使得 $(x+h)(x+6) \\equiv (x+4)^2+k$，則 $k =$
 <div class="q-mcopt">
-A.\t$-28$<br>
-B.\t$-16$<br>
-C.\t$-4$<br>
-D.\t$2$
+A.\t$-28$。<br>
+B.\t$-16$。<br>
+C.\t$-4$。<br>
+D.\t$2$。
 </div>`
     },
     {
@@ -121,10 +121,10 @@ D.\t$2$
         domain: ["Geometry"],
         topic: ["Equations of Straight Lines"],
         difficulty: 2,
-        content: `圖中，直線 $L_1$ 及直線 $L_2$ 的方程分別為 $x+ay+b=0$ 及 $bx+y+c=0$。下列何者正確？
+        content: `圖中，直線 $L_1$ 及直線 $L_2$ 的方程分別為 $x+ay+b=0$ 及 $bx+y+c=0$。 下列何者正確？
 <div class="q-mcms">
-I.\t$a<0$<br>
-II.\t$ab<1$<br>
+I.\t$a<0$
+II.\t$ab<1$
 III.\t$ac<b$
 </div>
 <div class="q-mcopt">
@@ -142,7 +142,7 @@ D.\tI、II 及 III
         domain: ["Algebra"],
         topic: ["Percentages"],
         difficulty: 2,
-        content: `某玩具的成本較其售價低 $x\\%$。售出該玩具後，盈利百分率為 $25\\%$。求 $x$。
+        content: `某玩具的成本較其售價低 $x\\%$。 售出該玩具後，盈利百分率為 $25\\%$。 求 $x$。 
 <div class="q-mcopt">
 A.\t$20$<br>
 B.\t$25$<br>
@@ -158,12 +158,12 @@ D.\t$80$
         domain: ["Geometry"],
         topic: ["Areas and Volumes"],
         difficulty: 2,
-        content: `某高爾夫球場的實際面積為 $0.75$ km$^2$。若該球場在某地圖上的面積為 $300$ cm$^2$，則該地圖的比例尺為
+        content: `某高爾夫球場的實際面積為 $0.75$ km$^2$。 若該球場在某地圖上的面積為 $300$ cm$^2$，則該地圖的比例尺為
 <div class="q-mcopt">
-A.\t$1:250$<br>
-B.\t$1:5000$<br>
-C.\t$1:62\\,500$<br>
-D.\t$1:25\\,000\\,000$
+A.\t$1:250$。<br>
+B.\t$1:5000$。<br>
+C.\t$1:62\\,500$。<br>
+D.\t$1:25\\,000\\,000$。
 </div>`
     },
     {
@@ -174,12 +174,12 @@ D.\t$1:25\\,000\\,000$
         domain: ["Algebra"],
         topic: ["Variations"],
         difficulty: 2,
-        content: `已知 $w$ 隨 $u$ 的立方及 $v$ 的平方根正變。當 $u=2$ 及 $v=4$ 時，$w=8$。當 $u=4$ 及 $v=9$ 時，$w=$
+        content: `已知 $w$ 隨 $u$ 的立方及 $v$ 的平方根正變。 當 $u=2$ 及 $v=4$ 時，$w=8$。 當 $u=4$ 及 $v=9$ 時，$w=$
 <div class="q-mcopt">
-A.\t$96$<br>
-B.\t$324$<br>
-C.\t$384$<br>
-D.\t$729$
+A.\t$96$。<br>
+B.\t$324$。<br>
+C.\t$384$。<br>
+D.\t$729$。
 </div>`
     },
     {
@@ -190,7 +190,7 @@ D.\t$729$
         domain: ["Algebra"],
         topic: ["Sequences"],
         difficulty: 1,
-        content: `圖中，第 $1$ 個圖案包含 $3$ 粒點子。對任意正整數 $n$，第 $(n+1)$ 個圖案是由第 $n$ 個圖案加上 $(2n+1)$ 粒點子所組成。求第 $7$ 個圖案的點子數目。
+        content: `圖中，第 $1$ 個圖案包含 $3$ 粒點子。 對任意正整數 $n$，第 $(n+1)$ 個圖案是由第 $n$ 個圖案加上 $(2n+1)$ 粒點子所組成。 求第 $7$ 個圖案的點子數目。 
 <div class="q-mcopt">
 A.\t$15$<br>
 B.\t$27$<br>
@@ -206,7 +206,7 @@ D.\t$51$
         domain: ["Algebra"],
         topic: ["Linear Inequalities and Compound Inequalities"],
         difficulty: 2,
-        content: `$5-4x<9$ 及 $\\dfrac{2x-3}{7}>1$ 的解為
+        content: `$5-4x<9$  及  $\\dfrac{2x-3}{7}>1$ 的解為
 <div class="q-mcopt">
 A.\t$x<-1$。<br>
 B.\t$x>-1$。<br>
@@ -222,7 +222,7 @@ D.\t$x>5$。
         domain: ["Algebra"],
         topic: ["Approximate Values and Numerical Estimation"],
         difficulty: 3,
-        content: `圖中，$PQRST$ 為五邊形，其中所有的量度均準確至最接近的 cm。設 $A$ cm$^2$ 為該五邊形的實際面積。求 $A$ 值的範圍。
+        content: `圖中，$PQRST$ 為五邊形，其中所有的量度均準確至最接近的 cm。 設 $A$ cm$^2$ 為該五邊形的實際面積。 求 $A$ 值的範圍。 
 <div class="q-mcopt">
 A.\t$27.83 \\le A < 31.83$<br>
 B.\t$44.75 \\le A < 60.75$<br>
@@ -238,7 +238,7 @@ D.\t$48.25 \\le A < 64.75$
         domain: ["Geometry"],
         topic: ["Areas and Volumes"],
         difficulty: 2,
-        content: `某扇形的角減少 $60\\%$，而它的半徑卻增加 $k\\%$。若該扇形的弧長維持不變，求 $k$ 的值。
+        content: `某扇形的角減少 $60\\%$，而它的半徑卻增加 $k\\%$。 若該扇形的弧長維持不變，求 $k$ 的值。 
 <div class="q-mcopt">
 A.\t$40$<br>
 B.\t$60$<br>
@@ -270,7 +270,7 @@ D.\t$735$ cm$^3$。
         domain: ["Geometry"],
         topic: ["Plane Geometry", "Areas and Volumes"],
         difficulty: 3,
-        content: `圖中，$P$ 及 $Q$ 均為 $OR$ 上的點而 $U$ 及 $T$ 均為 $OS$ 上的點使得 $OP = PQ = QR$ 且 $PU // QT // RS$。梯形 $PQTU$ 的面積與梯形 $QRST$ 的面積之比為
+        content: `圖中，$P$ 及 $Q$ 均為 $OR$ 上的點而 $U$ 及 $T$ 均為 $OS$ 上的點使得 $OP = PQ = QR$ 且 $PU // QT // RS$。 梯形 $PQTU$ 的面積與梯形 $QRST$ 的面積之比為
 <div class="q-mcopt">
 A.\t$1:2$。<br>
 B.\t$2:3$。<br>
@@ -286,12 +286,12 @@ D.\t$4:9$。
         domain: ["Geometry"],
         topic: ["Plane Geometry", "Areas and Volumes"],
         difficulty: 4,
-        content: `圖中，$ABCD$ 為一平行四邊形。設 $E$ 為 $AD$ 上的一點使得 $AE:ED = 2:5$。延長 $CB$ 至點 $F$ 使得 $BF = DE$。將 $AB$ 與 $EF$ 的交點記為 $G$。已知 $BD$ 與 $CG$ 相交於點 $H$。若 $\\Delta AEG$ 的面積為 $48$ cm$^2$，則 $\\Delta CDH$ 的面積為
+        content: `圖中，$ABCD$ 為一平行四邊形。 設 $E$ 為 $AD$ 上的一點使得 $AE:ED = 2:5$。 延長 $CB$ 至點 $F$ 使得 $BF = DE$。 將 $AB$ 與 $EF$ 的交點記為 $G$。 已知 $BD$ 與 $CG$ 相交於點 $H$。 若 $\\Delta AEG$ 的面積為 $48$ cm$^2$，則 $\\Delta CDH$ 的面積為
 <div class="q-mcopt">
-A.\t$98$ cm$^2$。<br>
-B.\t$343$ cm$^2$。<br>
-C.\t$420$ cm$^2$。<br>
-D.\t$588$ cm$^2$。
+A.\t$98$ cm$^2$。 <br>
+B.\t$343$ cm$^2$。 <br>
+C.\t$420$ cm$^2$。 <br>
+D.\t$588$ cm$^2$。 
 </div>`
     },
     {
@@ -304,8 +304,8 @@ D.\t$588$ cm$^2$。
         difficulty: 2,
         content: `根據圖中所示，下列何者必為正確？
 <div class="q-mcms">
-I.\t$u-v+w=0^\\circ$<br>
-II.\t$u+v-w=180^\\circ$<br>
+I.\t$u-v+w=0^\\circ$
+II.\t$u+v-w=180^\\circ$
 III.\t$u+v+w=450^\\circ$
 </div>
 <div class="q-mcopt">
@@ -323,7 +323,7 @@ D.\t只有 II 及 III
         domain: ["Geometry"],
         topic: ["Plane Geometry"],
         difficulty: 3,
-        content: `圖中，$ABC$ 為一等邊三角形及 $CDE$ 為一等腰三角形且 $CD=CE$。若 $\\angle DCE = 78^\\circ$ 及 $\\angle ADC = \\angle CAD = 40^\\circ$，則 $\\angle CBE =$
+        content: `圖中，$ABC$ 為一等邊三角形及 $CDE$ 為一等腰三角形且 $CD=CE$。 若 $\\angle DCE = 78^\\circ$ 及 $\\angle ADC = \\angle CAD = 40^\\circ$，則 $\\angle CBE =$
 <div class="q-mcopt">
 A.\t$14^\\circ$。<br>
 B.\t$19^\\circ$。<br>
@@ -339,7 +339,7 @@ D.\t$29^\\circ$。
         domain: ["Geometry"],
         topic: ["Plane Geometry"],
         difficulty: 2,
-        content: `圖中，$ABCD$ 為一長方形。設 $E$ 為 $AD$ 上的一點使得 $BE = 8$ cm 及 $CE = 15$ cm。若 $BC = 17$ cm，求長方形 $ABCD$ 的面積。
+        content: `圖中，$ABCD$ 為一長方形。 設 $E$ 為 $AD$ 上的一點使得 $BE = 8$ cm 及 $CE = 15$ cm。 若 $BC = 17$ cm，求長方形 $ABCD$ 的面積。 
 <div class="q-mcopt">
 A.\t$60$ cm$^2$<br>
 B.\t$68$ cm$^2$<br>
@@ -355,7 +355,7 @@ D.\t$136$ cm$^2$
         domain: ["Geometry"],
         topic: ["Circle Geometry"],
         difficulty: 3,
-        content: `圖中，$ABCDE$ 為一圓。若 $AB = 10$ cm、$BC = 5$ cm、$\\angle ABC = 90^\\circ$ 及 $\\angle CED = 40^\\circ$，求 $CD$ 準確至最接近的 cm。
+        content: `圖中，$ABCDE$ 為一圓。 若 $AB = 10$ cm 、 $BC = 5$ cm 、 $\\angle ABC = 90^\\circ$ 及 $\\angle CED = 40^\\circ$，求 $CD$ 準確至最接近的 cm。 
 <div class="q-mcopt">
 A.\t$5$ cm<br>
 B.\t$6$ cm<br>
@@ -371,7 +371,7 @@ D.\t$8$ cm
         domain: ["Geometry"],
         topic: ["Trigonometry (2D)"],
         difficulty: 2,
-        content: `某船位於燈塔以西 $50$ km。若該船沿 S$60^\\circ$E 方向移動，求該船與燈塔間的最短距離。
+        content: `某船位於燈塔以西 $50$ km。 若該船沿 S$60^\\circ$E 方向移動，求該船與燈塔間的最短距離。 
 <div class="q-mcopt">
 A.\t$20$ km<br>
 B.\t$25$ km<br>
@@ -387,12 +387,12 @@ D.\t$87$ km
         domain: ["Geometry"],
         topic: ["Coordinate Geometry"],
         difficulty: 2,
-        content: `點 $P$ 向左平移 $4$ 單位至點 $Q$。若 $Q$ 對 $y$ 軸的反射影像的坐標為 $(5, -1)$，則 $P$ 的極坐標為
+        content: `點 $P$ 向左平移 $4$ 單位至點 $Q$。 若 $Q$ 對 $y$ 軸的反射影像的坐標為 $(5, -1)$，則 $P$ 的極坐標為
 <div class="q-mcopt">
-A.\t$(1, 45^\\circ)$<br>
-B.\t$(1, 225^\\circ)$<br>
-C.\t$(\\sqrt{2}, 45^\\circ)$<br>
-D.\t$(\\sqrt{2}, 225^\\circ)$
+A.\t$(1, 45^\\circ)$。<br>
+B.\t$(1, 225^\\circ)$。<br>
+C.\t$(\\sqrt{2}, 45^\\circ)$。<br>
+D.\t$(\\sqrt{2}, 225^\\circ)$。
 </div>`
     },
     {
@@ -403,7 +403,7 @@ D.\t$(\\sqrt{2}, 225^\\circ)$
         domain: ["Geometry"],
         topic: ["Loci"],
         difficulty: 1,
-        content: `設 $A$ 為直線 $9x+4y-7=0$ 與直線 $9x-4y+7=0$ 的交點。若 $P$ 為直角坐標平面上的一動點使得 $P$ 與 $A$ 間的距離為 $8$，則 $P$ 的軌跡為一
+        content: `設 $A$ 為直線 $9x+4y-7=0$ 與直線 $9x-4y+7=0$ 的交點。 若 $P$ 為直角坐標平面上的一動點使得 $P$ 與 $A$ 間的距離為 $8$，則 $P$ 的軌跡為一
 <div class="q-mcopt">
 A.\t圓。<br>
 B.\t三角形。<br>
@@ -419,7 +419,7 @@ D.\t正六邊形。
         domain: ["Geometry"],
         topic: ["Equations of Straight Lines"],
         difficulty: 2,
-        content: `直線 $L$ 的方程為 $kx+4y-2k=0$，其中 $k$ 為一常數。若 $L$ 垂直於直線 $6x-9y+4=0$，求 $L$ 的 $y$ 截距。
+        content: `直線 $L$ 的方程為 $kx+4y-2k=0$，其中 $k$ 為一常數。 若 $L$ 垂直於直線 $6x-9y+4=0$，求 $L$ 的 $y$ 截距。 
 <div class="q-mcopt">
 A.\t$-3$<br>
 B.\t$-2$<br>
@@ -435,10 +435,10 @@ D.\t$3$
         domain: ["Geometry"],
         topic: ["Equations of Circles"],
         difficulty: 3,
-        content: `圓 $C_1$ 及圓 $C_2$ 的方程分別為 $2x^2+2y^2+4x+8y-149=0$ 及 $x^2+y^2-8x-20y-53=0$。下列何者正確？
+        content: `圓 $C_1$ 及圓 $C_2$ 的方程分別為 $2x^2+2y^2+4x+8y-149=0$ 及 $x^2+y^2-8x-20y-53=0$。 下列何者正確？
 <div class="q-mcms">
-I.\t$C_1$ 的圓心在 $C_2$ 上。<br>
-II.\t$C_1$ 與 $C_2$ 的半徑相等。<br>
+I.\t$C_1$ 的圓心在 $C_2$ 上。
+II.\t$C_1$ 與 $C_2$ 的半徑相等。
 III.\t$C_1$ 與 $C_2$ 相交於兩相異點。
 </div>
 <div class="q-mcopt">
@@ -456,7 +456,7 @@ D.\t只有 II 及 III
         domain: ["Data Handling"],
         topic: ["Probability"],
         difficulty: 1,
-        content: `從四張分別記有數字 $3$、$5$、$7$ 及 $9$ 的紙卡中，隨機同時抽出兩個數字。求抽出數字之積大於 $35$ 的概率。
+        content: `從四張分別記有數字 $3$、$5$、$7$ 及 $9$ 的紙卡中，隨機同時抽出兩個數字。 求抽出數字之積大於 $35$ 的概率。 
 <div class="q-mcopt">
 A.\t$\\dfrac{1}{2}$<br>
 B.\t$\\dfrac{1}{3}$<br>
@@ -472,7 +472,7 @@ D.\t$\\dfrac{3}{8}$
         domain: ["Data Handling"],
         topic: ["Measures of Dispersion", "Statistical Graphs"],
         difficulty: 1,
-        content: `下面的棒形圖顯示某些學生擁有原子筆的數目的分佈。求該分佈的四分位數間距。
+        content: `下面的棒形圖顯示某些學生擁有原子筆的數目的分佈。 求該分佈的四分位數間距。 
 <div class="q-mcopt">
 A.\t$1$<br>
 B.\t$2$<br>
@@ -490,10 +490,10 @@ D.\t$6$
         difficulty: 3,
         content: `考慮以下整數：
 $3$ $3$ $8$ $8$ $8$ $10$ $12$ $m$ $n$
-設 $x$、$y$ 及 $z$ 分別為以上整數的中位數、平均值及眾數。若以上整數的分佈域為 $9$，則下列何者必為正確？
+設 $x$、$y$ 及 $z$ 分別為以上整數的中位數、平均值及眾數。 若以上整數的分佈域為 $9$，則下列何者必為正確？
 <div class="q-mcms">
-I.\t$x=8$<br>
-II.\t$y=8$<br>
+I.\t$x=8$
+II.\t$y=8$
 III.\t$z=8$
 </div>
 <div class="q-mcopt">
@@ -511,7 +511,7 @@ D.\t只有 II 及 III
         domain: ["Algebra"],
         topic: ["Number Systems"],
         difficulty: 2,
-        content: `$\\mathrm{B}00000000000030_{16} =$
+        content: `$\\text{B00000000000030}_{16} =$
 <div class="q-mcopt">
 A.\t$10 \\times 2^{60} + 48$。<br>
 B.\t$11 \\times 2^{60} + 48$。<br>
@@ -543,10 +543,10 @@ D.\t$\\log_{\\pi}11$。
         domain: ["Algebra"],
         topic: ["Exponential Functions and Logarithm Functions"],
         difficulty: 2,
-        content: `圖中所示為在同一直角坐標系上 $y=a^x$ 的圖像及 $y=b^x$ 的圖像，其中 $a$ 及 $b$ 均為正常數。若 $y=a^x$ 的圖像為 $y=b^x$ 的圖像對 $y$ 軸的反射影像，則下列何者正確？
+        content: `圖中所示為在同一直角坐標系上 $y=a^x$ 的圖像及 $y=b^x$ 的圖像，其中 $a$ 及 $b$ 均為正常數。 若 $y=a^x$ 的圖像為 $y=b^x$ 的圖像對 $y$ 軸的反射影像，則下列何者正確？
 <div class="q-mcms">
-I.\t$a<1$<br>
-II.\t$b>1$<br>
+I.\t$a<1$
+II.\t$b>1$
 III.\t$ab=1$
 </div>
 <div class="q-mcopt">
@@ -564,7 +564,7 @@ D.\tI、II 及 III
         domain: ["Algebra"],
         topic: ["Functions and Graphs"],
         difficulty: 2,
-        content: `圖中的圖像顯示 $x^3$ 與 $\\sqrt{y}$ 之間的線性關係。若 $x=2$，則 $y=$
+        content: `圖中的圖像顯示 $x^3$ 與 $\\sqrt{y}$ 之間的線性關係。 若 $x=2$，則 $y=$
 <div class="q-mcopt">
 A.\t$3$。<br>
 B.\t$8$。<br>
@@ -582,8 +582,8 @@ D.\t$33$。
         difficulty: 2,
         content: `若 $a>0$，則下列何者為等差數列？
 <div class="q-mcms">
-I.\t$\\log a^{-3}, \\log a, \\log a^5$<br>
-II.\t$8-4a, 9-5a, 10-6a$<br>
+I.\t$\\log a^{-3}, \\log a, \\log a^5$
+II.\t$8-4a, 9-5a, 10-6a$
 III.\t$\\cos(90-a)^\\circ, \\cos 90^\\circ, \\cos(90+a)^\\circ$
 </div>
 <div class="q-mcopt">
@@ -609,7 +609,7 @@ $$
 x + y + 1 \\le 0
 \\end{cases}
 $$
-設 $D$ 為表示以上的不等式組的解之區域。求常數 $k$ 使得 $4x+3y+k$ 的最小值為 $24$，其中 $(x, y)$ 為 $D$ 中的一點。
+設 $D$ 為表示以上的不等式組的解之區域。 求常數 $k$ 使得 $4x+3y+k$ 的最小值為 $24$，其中 $(x, y)$ 為 $D$ 中的一點。 
 <div class="q-mcopt">
 A.\t$25$<br>
 B.\t$27$<br>
@@ -625,7 +625,7 @@ D.\t$53$
         domain: ["Algebra"],
         topic: ["Complex Numbers"],
         difficulty: 3,
-        content: `定義 $z_1 = \\dfrac{2+ki}{1+i}$ 及 $z_2 = \\dfrac{k+5i}{2-i}$，其中 $k$ 為一實數。若 $z_1$ 的虛部等於 $z_2$ 的虛部，則 $z_1 - z_2 =$
+        content: `定義 $z_1 = \\dfrac{2+ki}{1+i}$ 及 $z_2 = \\dfrac{k+5i}{2-i}$，其中 $k$ 為一實數。 若 $z_1$ 的虛部等於 $z_2$ 的虛部，則 $z_1 - z_2 =$
 <div class="q-mcopt">
 A.\t$-20$。<br>
 B.\t$0$。<br>
@@ -641,7 +641,7 @@ D.\t$10$。
         domain: ["Geometry"],
         topic: ["Trigonometry (3D)"],
         difficulty: 4,
-        content: `圖中，$ABCDEF$ 為一直立三角柱體。$P$ 為 $AE$ 上的一點。若 $AB = AC = 12$ cm、$AP = 9$ cm、$EP = 5$ cm 及 $BD = 2k$ cm，求 $\\Delta BDP$ 的面積。
+        content: `圖中，$ABCDEF$ 為一直立三角柱體。 $P$ 為 $AE$ 上的一點。 若 $AB = AC = 12$ cm 、 $AP = 9$ cm 、 $EP = 5$ cm 及 $BD = 2k$ cm，求 $\\Delta BDP$ 的面積。 
 <div class="q-mcopt">
 A.\t$\\sqrt{(k^2-1)(196-k^2)}$ cm$^2$<br>
 B.\t$\\sqrt{(k^2-1)(196+k^2)}$ cm$^2$<br>
@@ -657,7 +657,7 @@ D.\t$\\sqrt{(k^2+1)(196+k^2)}$ cm$^2$
         domain: ["Geometry"],
         topic: ["Circle Geometry"],
         difficulty: 3,
-        content: `圖中，$ABC$ 及 $CDE$ 均為圓使得 $ADC$ 為一直線。$PQ$ 為該兩圓在 $C$ 的公切線。$AB$ 為圓 $CDE$ 在 $E$ 的切線。若 $\\angle ADE = 100^\\circ$ 及 $\\angle BCQ = 35^\\circ$，則 $\\angle ABC =$
+        content: `圖中，$ABC$ 及 $CDE$ 均為圓使得 $ADC$ 為一直線。 $PQ$ 為該兩圓在 $C$ 的公切線。 $AB$ 為圓 $CDE$ 在 $E$ 的切線。 若 $\\angle ADE = 100^\\circ$ 及 $\\angle BCQ = 35^\\circ$，則 $\\angle ABC =$
 <div class="q-mcopt">
 A.\t$55^\\circ$。<br>
 B.\t$65^\\circ$。<br>
@@ -673,7 +673,7 @@ D.\t$80^\\circ$。
         domain: ["Geometry"],
         topic: ["Coordinate Geometry"],
         difficulty: 4,
-        content: `某三角形的三邊的方程為 $4x+3y=24$、$4x-3y=24$ 及 $x=a$，其中 $a$ 為一常數。若該三角形的內心的 $x$ 坐標為 $31$，則 $a=$
+        content: `某三角形的三邊的方程為 $4x+3y=24$ 、 $4x-3y=24$ 及 $x=a$，其中 $a$ 為一常數。 若該三角形的內心的 $x$ 坐標為 $31$，則 $a=$
 <div class="q-mcopt">
 A.\t$15$。<br>
 B.\t$31$。<br>
@@ -689,12 +689,12 @@ D.\t$51$。
         domain: ["Geometry"],
         topic: ["Equations of Circles"],
         difficulty: 3,
-        content: `求 $c$ 值的範圍使得圓 $x^2+y^2-6x+cy-7=0$ 與直線 $x-y+9=0$ 相交。
+        content: `求 $c$ 值的範圍使得圓 $x^2+y^2-6x+cy-7=0$ 與直線 $x-y+9=0$ 相交。 
 <div class="q-mcopt">
 A.\t$-56 \\le c \\le 8$<br>
 B.\t$-8 \\le c \\le 56$<br>
-C.\t$c \\le -56$ 或 $c \\ge 8$<br>
-D.\t$c \\le -8$ 或 $c \\ge 56$
+C.\t$c \\le -56$  或  $c \\ge 8$<br>
+D.\t$c \\le -8$  或  $c \\ge 56$
 </div>`
     },
     {
@@ -705,7 +705,7 @@ D.\t$c \\le -8$ 或 $c \\ge 56$
         domain: ["Data Handling"],
         topic: ["Permutations and Combinations"],
         difficulty: 2,
-        content: `$6$ 名男生與 $5$ 名女生排成一隊。若沒有男生相鄰而排，則可排成多少不同的隊？
+        content: `$6$ 名男生與 $5$ 名女生排成一隊。 若沒有男生相鄰而排，則可排成多少不同的隊？
 <div class="q-mcopt">
 A.\t$86400$<br>
 B.\t$172800$<br>
@@ -721,7 +721,7 @@ D.\t$39916800$
         domain: ["Data Handling"],
         topic: ["Probability"],
         difficulty: 3,
-        content: `某箱子內有 $8$ 本中文書及 $7$ 本英文書。若從該箱子中隨機同時抽出 $5$ 本書，求抽出至多 $3$ 本中文書的概率。
+        content: `某箱子內有 $8$ 本中文書及 $7$ 本英文書。 若從該箱子中隨機同時抽出 $5$ 本書，求抽出至多 $3$ 本中文書的概率。 
 <div class="q-mcopt">
 A.\t$\\dfrac{2}{11}$<br>
 B.\t$\\dfrac{9}{11}$<br>
@@ -737,7 +737,7 @@ D.\t$\\dfrac{82}{143}$
         domain: ["Data Handling"],
         topic: ["Measures of Dispersion"],
         difficulty: 2,
-        content: `在一測驗，某兩學生的測驗得分之差及標準分之差分別為 $30$ 分及 $6$。在該測驗，測驗得分的標準差為
+        content: `在一測驗，某兩學生的測驗得分之差及標準分之差分別為 $30$ 分及 $6$。 在該測驗，測驗得分的標準差為
 <div class="q-mcopt">
 A.\t$5$ 分。<br>
 B.\t$24$ 分。<br>
@@ -753,7 +753,7 @@ D.\t$36$ 分。
         domain: ["Data Handling"],
         topic: ["Measures of Dispersion"],
         difficulty: 2,
-        content: `六個數 $20a+3$、$20a+5$、$20a+9$、$20a+11$、$20a+15$ 及 $20a+17$ 的方差為
+        content: `六個數 $20a+3$ 、 $20a+5$ 、 $20a+9$ 、 $20a+11$ 、 $20a+15$ 及 $20a+17$ 的方差為
 <div class="q-mcopt">
 A.\t$5$。<br>
 B.\t$10$。<br>
