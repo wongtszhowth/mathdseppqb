e6f9c785534c4c1ed985e830051945a0597d8476
TH Wong <wongth@chusheklun.edu.hk>
2026-07-30 11:21:46 +0800

    Update 2019dsep2.js

--- 2019dsep2.js
+++ 2019dsep2.js
@@ -75,10 +75,10 @@ D.\t$-6$
         difficulty: 1,
         content: `若 $h=3-\\dfrac{5}{k+4}$，則 $k=$
 <div class="q-mcopt">
-A.\t$\\dfrac{4h-7}{3-h}$。<br>
-B.\t$\\dfrac{4h-17}{3-h}$。<br>
-C.\t$\\dfrac{4h-7}{3+h}$。<br>
-D.\t$\\dfrac{4h-17}{3+h}$。
+A.\t$\\dfrac{4h-7}{3-h}$。 <br>
+B.\t$\\dfrac{4h-17}{3-h}$。 <br>
+C.\t$\\dfrac{4h-7}{3+h}$。 <br>
+D.\t$\\dfrac{4h-17}{3+h}$。 
 </div>`
     },
     {
@@ -107,10 +107,10 @@ D.\t$x=0.0656$（準確至三位有效數字）
         difficulty: 2,
         content: `滿足複合不等式 $-2(x-5)+5 < 21$ 或 $\\dfrac{3x-5}{-7} > 1$ 的最小整數為
 <div class="q-mcopt">
-A.\t$-3$。<br>
-B.\t$-2$。<br>
-C.\t$4$。<br>
-D.\t$5$。
+A.\t$-3$。 <br>
+B.\t$-2$。 <br>
+C.\t$4$。 <br>
+D.\t$5$。 
 </div>`
     },
     {
@@ -121,12 +121,12 @@ D.\t$5$。
         domain: ["Algebra"],
         topic: ["Functions and Graphs"],
         difficulty: 2,
-        content: `設 $c$ 為一常數。若 $\\mathrm{f}(x)=x^3+cx^2+c$，則 $\\mathrm{f}(c)+\\mathrm{f}(-c)=$
+        content: `設 $c$ 為一常數。 若 $\\mathrm{f}(x)=x^3+cx^2+c$，則 $\\mathrm{f}(c)+\\mathrm{f}(-c)=$
 <div class="q-mcopt">
-A.\t$0$。<br>
-B.\t$2c$。<br>
-C.\t$2c^3+2c$。<br>
-D.\t$-2c^3+2c$。
+A.\t$0$。 <br>
+B.\t$2c$。 <br>
+C.\t$2c^3+2c$。 <br>
+D.\t$-2c^3+2c$。 
 </div>`
     },
     {
@@ -137,7 +137,7 @@ D.\t$-2c^3+2c$。
         domain: ["Algebra"],
         topic: ["Polynomials"],
         difficulty: 2,
-        content: `設 $k$ 為一常數使得 $2x^4 + kx^3 - 4x - 16$ 可被 $2x + k$ 整除。求 $k$。
+        content: `設 $k$ 為一常數使得 $2x^4 + kx^3 - 4x - 16$ 可被 $2x + k$ 整除。 求 $k$。 
 <div class="q-mcopt">
 A.\t$-2$<br>
 B.\t$2$<br>
@@ -155,8 +155,8 @@ D.\t$8$
         difficulty: 2,
         content: `下列有關 $y = (3-x)(x+2)+6$ 的圖像之敍述，何者正確？
 <div class="q-mcms">
-I.\t該圖像開口向下。<br>
-II.\t該圖像通過點 $(1,10)$。<br>
+I.\t該圖像開口向下。
+II.\t該圖像通過點 $(1,10)$。
 III.\t該圖像的 $x$ 截距為 $-2$ 及 $3$。
 </div>
 <div class="q-mcopt">
@@ -174,7 +174,7 @@ D.\t只有 II 及 III
         domain: ["Algebra"],
         topic: ["Percentages"],
         difficulty: 2,
-        content: `存款 $\\$65\\,000$，年利率 $7\\%$，年期 $8$ 年，複利計算，每季一結。求本利和準確至最接近的元。
+        content: `存款 $\\$65\\,000$，年利率 $7\\%$，年期 $8$ 年，複利計算，每季一結。 求本利和準確至最接近的元。 
 <div class="q-mcopt">
 A.\t$\\$101\\,400$<br>
 B.\t$\\$111\\,682$<br>
@@ -190,12 +190,12 @@ D.\t$\\$113\\,609$
         domain: ["Algebra"],
         topic: ["Rates, Ratios and Proportions"],
         difficulty: 2,
-        content: `A 牌及 B 牌茶葉的成本分別為 $\\$140/$kg 及 $\\$315/$kg。若 $x$ kg 的 A 牌茶葉與 $y$ kg 的 B 牌茶葉混合使得混合後的茶葉的成本為 $\\$210/$kg，則 $x:y=$
+        content: `A 牌及 B 牌茶葉的成本分別為 $\\$140/$kg 及 $\\$315/$kg。 若 $x$ kg 的 A 牌茶葉與 $y$ kg 的 B 牌茶葉混合使得混合後的茶葉的成本為 $\\$210/$kg，則 $x:y=$
 <div class="q-mcopt">
-A.\t$2:3$。<br>
-B.\t$3:2$。<br>
-C.\t$4:9$。<br>
-D.\t$9:4$。
+A.\t$2:3$。 <br>
+B.\t$3:2$。 <br>
+C.\t$4:9$。 <br>
+D.\t$9:4$。 
 </div>`
     },
     {
@@ -206,12 +206,12 @@ D.\t$9:4$。
         domain: ["Algebra"],
         topic: ["Variations"],
         difficulty: 2,
-        content: `已知 $z$ 隨 $x$ 的平方正變且隨 $y$ 的平方根反變。若 $x$ 減少 $40\\%$ 且 $y$ 增加 $44\\%$，則 $z$
+        content: `已知 $z$ 隨 $x$ 的平方正變且隨 $y$ 的平方根反變。 若 $x$ 減少 $40\\%$ 且 $y$ 增加 $44\\%$，則 $z$
 <div class="q-mcopt">
-A.\t減少 $70\\%$。<br>
-B.\t增加 $70\\%$。<br>
-C.\t減少 $76\\%$。<br>
-D.\t增加 $76\\%$。
+A.\t減少 $70\\%$。 <br>
+B.\t增加 $70\\%$。 <br>
+C.\t減少 $76\\%$。 <br>
+D.\t增加 $76\\%$。 
 </div>`
     },
     {
@@ -222,7 +222,7 @@ D.\t增加 $76\\%$。
         domain: ["Algebra"],
         topic: ["Sequences"],
         difficulty: 1,
-        content: `圖中，第 $1$ 個圖案包含 $6$ 粒點子。對任意正整數 $n$，第 $(n+1)$ 個圖案是由第 $n$ 個圖案加上 $4$ 粒點子所組成。求第 $9$ 個圖案的點子數目。
+        content: `圖中，第 $1$ 個圖案包含 $6$ 粒點子。 對任意正整數 $n$，第 $(n+1)$ 個圖案是由第 $n$ 個圖案加上 $4$ 粒點子所組成。 求第 $9$ 個圖案的點子數目。 
 <div class="q-mcopt">
 A.\t$30$<br>
 B.\t$34$<br>
@@ -238,12 +238,12 @@ D.\t$42$
         domain: ["Geometry"],
         topic: ["Areas and Volumes"],
         difficulty: 2,
-        content: `某實心直立角錐體的底為一正方形，其邊長為 $18$ cm。若該角錐體的高為 $12$ cm，則該角錐體的總表面面積為
+        content: `某實心直立角錐體的底為一正方形，其邊長為 $18$ cm。 若該角錐體的高為 $12$ cm，則該角錐體的總表面面積為
 <div class="q-mcopt">
-A.\t$432$ cm$^2$。<br>
-B.\t$540$ cm$^2$。<br>
-C.\t$756$ cm$^2$。<br>
-D.\t$864$ cm$^2$。
+A.\t$432$ cm$^2$。 <br>
+B.\t$540$ cm$^2$。 <br>
+C.\t$756$ cm$^2$。 <br>
+D.\t$864$ cm$^2$。 
 </div>`
     },
     {
@@ -254,12 +254,12 @@ D.\t$864$ cm$^2$。
         domain: ["Geometry"],
         topic: ["Plane Geometry", "Areas and Volumes"],
         difficulty: 3,
-        content: `圖中，$ABCD$ 為一平行四邊形及 $AEFG$ 為一正方形。已知 $BE:EF:FG = 2:7:3$。$BD$ 分別與 $AE$ 及 $FG$ 相交於點 $X$ 及點 $Y$。若 $\\triangle ABX$ 的面積為 $24$ cm$^2$，則四邊形 $CDYF$ 的面積為
+        content: `圖中，$ABCD$ 為一平行四邊形及 $AEFG$ 為一正方形。 已知 $BE:EF:FG = 2:7:3$。 $BD$ 分別與 $AE$ 及 $FG$ 相交於點 $X$ 及點 $Y$。 若 $\\Delta ABX$ 的面積為 $24$ cm$^2$，則四邊形 $CDYF$ 的面積為
 <div class="q-mcopt">
-A.\t$54$ cm$^2$。<br>
-B.\t$77$ cm$^2$。<br>
-C.\t$81$ cm$^2$。<br>
-D.\t$87$ cm$^2$。
+A.\t$54$ cm$^2$。 <br>
+B.\t$77$ cm$^2$。 <br>
+C.\t$81$ cm$^2$。 <br>
+D.\t$87$ cm$^2$。 
 </div>`
     },
     {
@@ -270,12 +270,12 @@ D.\t$87$ cm$^2$。
         domain: ["Geometry"],
         topic: ["Plane Geometry"],
         difficulty: 2,
-        content: `圖中，$ABC$ 及 $ADE$ 均為直線。已知 $AB = BD$ 及 $BC = CD$。若 $\\angle CDE = 66^\\circ$，則 $\\angle ACD =$
+        content: `圖中，$ABC$ 及 $ADE$ 均為直線。 已知 $AB = BD$ 及 $BC = CD$。 若 $\\angle CDE = 66^\\circ$，則 $\\angle ACD =$
 <div class="q-mcopt">
-A.\t$28^\\circ$。<br>
-B.\t$33^\\circ$。<br>
-C.\t$36^\\circ$。<br>
-D.\t$38^\\circ$。
+A.\t$28^\\circ$。 <br>
+B.\t$33^\\circ$。 <br>
+C.\t$36^\\circ$。 <br>
+D.\t$38^\\circ$。 
 </div>`
     },
     {
@@ -286,12 +286,12 @@ D.\t$38^\\circ$。
         domain: ["Geometry"],
         topic: ["Plane Geometry"],
         difficulty: 3,
-        content: `圖中，$ABC$ 為一等腰三角形且 $AB = AC$。$D$ 及 $E$ 均為 $AB$ 上的點使得 $AD = DE = 2EB$，而 $F$ 為 $AC$ 上的一點使得 $DF//EC$。若 $\\angle ADF = 90^\\circ$ 及 $CE = 60$ cm，則 $EF =$
+        content: `圖中，$ABC$ 為一等腰三角形且 $AB = AC$。 $D$ 及 $E$ 均為 $AB$ 上的點使得 $AD = DE = 2EB$，而 $F$ 為 $AC$ 上的一點使得 $DF//EC$。 若 $\\angle ADF = 90^\\circ$ 及 $CE = 60$ cm，則 $EF =$
 <div class="q-mcopt">
-A.\t$40$ cm。<br>
-B.\t$45$ cm。<br>
-C.\t$48$ cm。<br>
-D.\t$50$ cm。
+A.\t$40$ cm。 <br>
+B.\t$45$ cm。 <br>
+C.\t$48$ cm。 <br>
+D.\t$50$ cm。 
 </div>`
     },
     {
@@ -302,7 +302,7 @@ D.\t$50$ cm。
         domain: ["Geometry"],
         topic: ["Plane Geometry", "Areas and Volumes"],
         difficulty: 3,
-        content: `圖中，$ABCD$ 為一梯形且 $AB//DC$ 及 $\\angle ABD = 90^\\circ$。若 $AB = 18$ cm、$BC = 26$ cm 及 $AD = 30$ cm，求梯形 $ABCD$ 的面積。
+        content: `圖中，$ABCD$ 為一梯形且 $AB//DC$ 及 $\\angle ABD = 90^\\circ$。 若 $AB = 18$ cm、$BC = 26$ cm 及 $AD = 30$ cm，求梯形 $ABCD$ 的面積。 
 <div class="q-mcopt">
 A.\t$336$ cm$^2$<br>
 B.\t$400$ cm$^2$<br>
@@ -318,12 +318,12 @@ D.\t$552$ cm$^2$
         domain: ["Geometry"],
         topic: ["Plane Geometry"],
         difficulty: 3,
-        content: `圖中，$ABCD$ 為一菱形。$ABE$ 及 $BCF$ 均為直線使得 $BE = EF$。若 $\\angle BEF = 56^\\circ$，則 $\\angle BDC =$
+        content: `圖中，$ABCD$ 為一菱形。 $ABE$ 及 $BCF$ 均為直線使得 $BE = EF$。 若 $\\angle BEF = 56^\\circ$，則 $\\angle BDC =$
 <div class="q-mcopt">
-A.\t$48^\\circ$。<br>
-B.\t$56^\\circ$。<br>
-C.\t$59^\\circ$。<br>
-D.\t$62^\\circ$。
+A.\t$48^\\circ$。 <br>
+B.\t$56^\\circ$。 <br>
+C.\t$59^\\circ$。 <br>
+D.\t$62^\\circ$。 
 </div>`
     },
     {
@@ -334,12 +334,12 @@ D.\t$62^\\circ$。
         domain: ["Geometry"],
         topic: ["Circle Geometry"],
         difficulty: 3,
-        content: `圖中，$O$ 為半圓 $ABCD$ 的圓心。若 $AC = BD$ 及 $\\angle COD = 48^\\circ$，則 $\\angle ABD =$
+        content: `圖中，$O$ 為半圓 $ABCD$ 的圓心。 若 $AC = BD$ 及 $\\angle COD = 48^\\circ$，則 $\\angle ABD =$
 <div class="q-mcopt">
-A.\t$31^\\circ$。<br>
-B.\t$33^\\circ$。<br>
-C.\t$42^\\circ$。<br>
-D.\t$48^\\circ$。
+A.\t$31^\\circ$。 <br>
+B.\t$33^\\circ$。 <br>
+C.\t$42^\\circ$。 <br>
+D.\t$48^\\circ$。 
 </div>`
     },
     {
@@ -350,7 +350,7 @@ D.\t$48^\\circ$。
         domain: ["Geometry"],
         topic: ["Trigonometry (2D)"],
         difficulty: 2,
-        content: `圖中，$ABCD$ 為一長方形。$E$ 為 $AD$ 上的一點。求 $\\dfrac{CE}{AC}$。
+        content: `圖中，$ABCD$ 為一長方形。 $E$ 為 $AD$ 上的一點。 求 $\\dfrac{CE}{AC}$。 
 <div class="q-mcopt">
 A.\t$\\dfrac{\\sin \\alpha}{\\sin \\beta}$<br>
 B.\t$\\dfrac{\\cos \\alpha}{\\cos \\beta}$<br>
@@ -366,10 +366,10 @@ D.\t$\\cos \\alpha \\cos \\beta$
         domain: ["Geometry"],
         topic: ["Equations of Straight Lines"],
         difficulty: 2,
-        content: `圖中，直線 $L$ 的方程為 $ax+by+15=0$。下列何者正確？
+        content: `圖中，直線 $L$ 的方程為 $ax+by+15=0$。 下列何者正確？
 <div class="q-mcms">
-I.\t$a > b$<br>
-II.\t$a > -3$<br>
+I.\t$a > b$
+II.\t$a > -3$
 III.\t$b > -5$
 </div>
 <div class="q-mcopt">
@@ -387,7 +387,7 @@ D.\tI、II 及 III
         domain: ["Geometry"],
         topic: ["Equations of Straight Lines"],
         difficulty: 1,
-        content: `求常數 $k$ 使得直線 $3x+2y+k=0$ 與直線 $kx+12y-6=0$ 互相垂直。
+        content: `求常數 $k$ 使得直線 $3x+2y+k=0$ 與直線 $kx+12y-6=0$ 互相垂直。 
 <div class="q-mcopt">
 A.\t$-8$<br>
 B.\t$-4$<br>
@@ -403,7 +403,7 @@ D.\t$8$
         domain: ["Geometry"],
         topic: ["Coordinate Geometry"],
         difficulty: 2,
-        content: `點 $A$ 的坐標為 $(-5, -2)$。$A$ 向右平移 $9$ 單位至點 $B$，然後 $B$ 繞原點逆時針方向旋轉 $90^\\circ$ 至點 $C$。求 $C$ 的 $y$ 坐標。
+        content: `點 $A$ 的坐標為 $(-5, -2)$。 $A$ 向右平移 $9$ 單位至點 $B$，然後 $B$ 繞原點逆時針方向旋轉 $90^\\circ$ 至點 $C$。 求 $C$ 的 $y$ 坐標。 
 <div class="q-mcopt">
 A.\t$-4$<br>
 B.\t$-2$<br>
@@ -419,12 +419,12 @@ D.\t$4$
         domain: ["Geometry"],
         topic: ["Loci"],
         difficulty: 2,
-        content: `直線 $L$ 的方程為 $5x - 7y - 14 = 0$。若 $P$ 為直角坐標平面上的一動點使得由 $P$ 至 $L$ 的垂直距離等於 $3$，則 $P$ 的軌跡為
+        content: `直線 $L$ 的方程為 $5x - 7y - 14 = 0$。 若 $P$ 為直角坐標平面上的一動點使得由 $P$ 至 $L$ 的垂直距離等於 $3$，則 $P$ 的軌跡為
 <div class="q-mcopt">
-A.\t一扇形。<br>
-B.\t一正方形。<br>
-C.\t一拋物線。<br>
-D.\t一對直線。
+A.\t一扇形。 <br>
+B.\t一正方形。 <br>
+C.\t一拋物線。 <br>
+D.\t一對直線。 
 </div>`
     },
     {
@@ -435,10 +435,10 @@ D.\t一對直線。
         domain: ["Geometry"],
         topic: ["Equations of Circles"],
         difficulty: 3,
-        content: `將圓 $2x^2 + 2y^2 + 4x - 12y + 15 = 0$ 記為 $C$。下列何者正確？
+        content: `將圓 $2x^2 + 2y^2 + 4x - 12y + 15 = 0$ 記為 $C$。 下列何者正確？
 <div class="q-mcms">
-I.\t$C$ 的面積為 $25\\pi$。<br>
-II.\t點 $(-3, 3)$ 位於 $C$ 以外。<br>
+I.\t$C$ 的面積為 $25\\pi$。
+II.\t點 $(-3, 3)$ 位於 $C$ 以外。
 III.\t$C$ 的圓心位於第四象限。
 </div>
 <div class="q-mcopt">
@@ -456,7 +456,7 @@ D.\t只有 II 及 III
         domain: ["Data Handling"],
         topic: ["Probability"],
         difficulty: 1,
-        content: `從九個分別記有數字 $1$、$2$、$3$、$4$、$5$、$6$、$7$、$8$ 及 $9$ 的球中，隨機同時抽出兩個數字。求抽出的兩個數字為連續整數的概率。
+        content: `從九個分別記有數字 $1$、$2$、$3$、$4$、$5$、$6$、$7$、$8$ 及 $9$ 的球中，隨機同時抽出兩個數字。 求抽出的兩個數字為連續整數的概率。 
 <div class="q-mcopt">
 A.\t$\\dfrac{1}{2}$<br>
 B.\t$\\dfrac{1}{4}$<br>
@@ -474,8 +474,8 @@ D.\t$\\dfrac{7}{9}$
         difficulty: 1,
         content: `下列何者能從任意一框線圖得出？
 <div class="q-mcms">
-I.\t分佈域<br>
-II.\t標準差<br>
+I.\t分佈域
+II.\t標準差
 III.\t四分位數間距
 </div>
 <div class="q-mcopt">
@@ -493,14 +493,14 @@ D.\tI、II 及 III
         domain: ["Data Handling"],
         topic: ["Measures of Central Tendency"],
         difficulty: 2,
-        content: `下表顯示某些學生在一年內所獲優點的數目的分佈。
+        content: `下表顯示某些學生在一年內所獲優點的數目的分佈。 
 <div style="overflow-x: auto; width: 100%; margin-bottom: 8px;"><table class="q-table" style="width: 100%; min-width: 300px;"><tr><td>所獲優點的數目</td><td>$6$</td><td>$7$</td><td>$8$</td><td>$9$</td><td>$10$</td></tr><tr><td>學生人數</td><td>$32$</td><td>$36$</td><td>$28$</td><td>$18$</td><td>$2$</td></tr></table></div>
 下列何者正確？
 <div class="q-mcopt">
-A.\t該分佈的眾數為 $36$。<br>
-B.\t該分佈的中位數為 $8$。<br>
-C.\t該分佈的下四分位數為 $6$。<br>
-D.\t該分佈的上四分位數為 $10$。
+A.\t該分佈的眾數為 $36$。 <br>
+B.\t該分佈的中位數為 $8$。 <br>
+C.\t該分佈的下四分位數為 $6$。 <br>
+D.\t該分佈的上四分位數為 $10$。 
 </div>`
     },
     {
@@ -511,7 +511,7 @@ D.\t該分佈的上四分位數為 $10$。
         domain: ["Algebra"],
         topic: ["Exponential Functions and Logarithm Functions"],
         difficulty: 3,
-        content: `已知 $\\log_9 y$ 為 $\\log_3 x$ 的線性函數。該線性函數的圖像在垂直軸上的截距及在水平軸上的截距分別為 $7$ 及 $8$。下列何者必為正確？
+        content: `已知 $\\log_9 y$ 為 $\\log_3 x$ 的線性函數。 該線性函數的圖像在垂直軸上的截距及在水平軸上的截距分別為 $7$ 及 $8$。 下列何者必為正確？
 <div class="q-mcopt">
 A.\t$x^4y^7 = 3^{56}$<br>
 B.\t$x^7y^4 = 3^{56}$<br>
@@ -529,10 +529,10 @@ D.\t$x^8y^7 = 3^{56}$
         difficulty: 3,
         content: `若 $\\dfrac{3}{3\\log x - 2} + 7 = \\dfrac{2}{2\\log x + 1}$，則 $\\log \\dfrac{1}{x} =$
 <div class="q-mcopt">
-A.\t$-3$ 或 $2$。<br>
-B.\t$-2$ 或 $3$。<br>
-C.\t$\\dfrac{-1}{3}$ 或 $\\dfrac{1}{2}$。<br>
-D.\t$\\dfrac{-1}{2}$ 或 $\\dfrac{1}{3}$。
+A.\t$-3$ 或 $2$。 <br>
+B.\t$-2$ 或 $3$。 <br>
+C.\t$\\dfrac{-1}{3}$ 或 $\\dfrac{1}{2}$。 <br>
+D.\t$\\dfrac{-1}{2}$ 或 $\\dfrac{1}{3}$。 
 </div>`
     },
     {
@@ -545,10 +545,10 @@ D.\t$\\dfrac{-1}{2}$ 或 $\\dfrac{1}{3}$。
         difficulty: 2,
         content: `$100110000010110_2 =$
 <div class="q-mcopt">
-A.\t$19 \\times 2^{10} + 22$。<br>
-B.\t$19 \\times 2^{10} + 44$。<br>
-C.\t$19 \\times 2^{11} + 22$。<br>
-D.\t$19 \\times 2^{11} + 44$。
+A.\t$19 \\times 2^{10} + 22$。 <br>
+B.\t$19 \\times 2^{10} + 44$。 <br>
+C.\t$19 \\times 2^{11} + 22$。 <br>
+D.\t$19 \\times 2^{11} + 44$。 
 </div>`
     },
     {
@@ -561,10 +561,10 @@ D.\t$19 \\times 2^{11} + 44$。
         difficulty: 3,
         content: `若 $a$ 為一實數，則 $\\dfrac{4+i^5}{a+i}$ 的實部為
 <div class="q-mcopt">
-A.\t$\\dfrac{4a+1}{a^2-1}$。<br>
-B.\t$\\dfrac{4a+1}{a^2+1}$。<br>
-C.\t$\\dfrac{a^2+4a+2}{a^2-1}$。<br>
-D.\t$\\dfrac{a^2+4a+2}{a^2+1}$。
+A.\t$\\dfrac{4a+1}{a^2-1}$。 <br>
+B.\t$\\dfrac{4a+1}{a^2+1}$。 <br>
+C.\t$\\dfrac{a^2+4a+2}{a^2-1}$。 <br>
+D.\t$\\dfrac{a^2+4a+2}{a^2+1}$。 
 </div>`
     },
     {
@@ -583,12 +583,12 @@ x+2y \\le 20 \\\\
 13x+6y \\ge 20
 \\end{cases}
 $$
-設 $R$ 為表示以上的不等式組的解之區域。若 $(x, y)$ 為 $R$ 中的一點，則 $7x+8y+9$ 的最大值為
+設 $R$ 為表示以上的不等式組的解之區域。 若 $(x, y)$ 為 $R$ 中的一點，則 $7x+8y+9$ 的最大值為
 <div class="q-mcopt">
-A.\t$15$。<br>
-B.\t$77$。<br>
-C.\t$113$。<br>
-D.\t$115$。
+A.\t$15$。 <br>
+B.\t$77$。 <br>
+C.\t$113$。 <br>
+D.\t$115$。 
 </div>`
     },
     {
@@ -599,7 +599,7 @@ D.\t$115$。
         domain: ["Algebra"],
         topic: ["Sequences"],
         difficulty: 3,
-        content: `一等比數列的第 $2$ 項與第 $5$ 項之和為 $9$，而該數列的第 $7$ 項與第 $10$ 項之和為 $288$。求該數列的第 $20$ 項。
+        content: `一等比數列的第 $2$ 項與第 $5$ 項之和為 $9$，而該數列的第 $7$ 項與第 $10$ 項之和為 $288$。 求該數列的第 $20$ 項。 
 <div class="q-mcopt">
 A.\t$65536$<br>
 B.\t$131072$<br>
@@ -615,7 +615,7 @@ D.\t$524288$
         domain: ["Geometry"],
         topic: ["Equations of Circles", "Equations of Straight Lines"],
         difficulty: 3,
-        content: `設 $k$ 為一常數。直線 $3x-y-2=0$ 與圓 $5x^2+5y^2+kx+4y-20=0$ 相交於點 $P$ 及點 $Q$。若 $PQ$ 的中點的 $x$ 坐標為 $2$，求 $k$。
+        content: `設 $k$ 為一常數。 直線 $3x-y-2=0$ 與圓 $5x^2+5y^2+kx+4y-20=0$ 相交於點 $P$ 及點 $Q$。 若 $PQ$ 的中點的 $x$ 坐標為 $2$，求 $k$。 
 <div class="q-mcopt">
 A.\t$-152$<br>
 B.\t$-52$<br>
@@ -631,7 +631,7 @@ D.\t$248$
         domain: ["Geometry"],
         topic: ["Areas and Volumes"],
         difficulty: 3,
-        content: `圖中，$O$ 為扇形 $OABC$ 的圓心。已知 $\\triangle OAB$ 為一等邊三角形，$AC$ 與 $OB$ 相交於點 $D$。若 $OA=12$ cm 及 $\\angle AOC=90^\\circ$，求陰影區域 $BCD$ 的面積準確至最接近的 cm$^2$。
+        content: `圖中，$O$ 為扇形 $OABC$ 的圓心。 已知 $\\Delta OAB$ 為一等邊三角形，$AC$ 與 $OB$ 相交於點 $D$。 若 $OA=12$ cm 及 $\\angle AOC=90^\\circ$，求陰影區域 $BCD$ 的面積準確至最接近的 cm$^2$。 
 <div class="q-mcopt">
 A.\t$11$ cm$^2$<br>
 B.\t$16$ cm$^2$<br>
@@ -647,7 +647,7 @@ D.\t$38$ cm$^2$
         domain: ["Geometry"],
         topic: ["Circle Geometry"],
         difficulty: 3,
-        content: `圖中，$TA$ 為圓 $ABCDE$ 在點 $A$ 的切線。若 $\\angle BAD=64^\\circ$、$\\angle EAT=38^\\circ$ 及 $\\angle DCE=22^\\circ$，則 $\\angle ADB=$
+        content: `圖中，$TA$ 為圓 $ABCDE$ 在點 $A$ 的切線。 若 $\\angle BAD=64^\\circ$、$\\angle EAT=38^\\circ$ 及 $\\angle DCE=22^\\circ$，則 $\\angle ADB=$
 <div class="q-mcopt">
 A.\t$52^\\circ$<br>
 B.\t$56^\\circ$<br>
@@ -663,7 +663,7 @@ D.\t$68^\\circ$
         domain: ["Geometry"],
         topic: ["Trigonometry (3D)"],
         difficulty: 4,
-        content: `圖中所示為四面體 $PQRS$，其底 $QRS$ 在水平地面上。已知 $Q$ 鉛垂於 $P$ 之下。若 $\\angle PRQ = 47^\\circ$、$\\angle PSQ = 53^\\circ$ 及 $\\angle RQS = 120^\\circ$，求 $\\angle RPS$ 準確至最接近的度。
+        content: `圖中所示為四面體 $PQRS$，其底 $QRS$ 在水平地面上。 已知 $Q$ 鉛垂於 $P$ 之下。 若 $\\angle PRQ = 47^\\circ$、$\\angle PSQ = 53^\\circ$ 及 $\\angle RQS = 120^\\circ$，求 $\\angle RPS$ 準確至最接近的度。 
 <div class="q-mcopt">
 A.\t$52^\\circ$<br>
 B.\t$60^\\circ$<br>
@@ -679,11 +679,11 @@ D.\t$76^\\circ$
         domain: ["Geometry"],
         topic: ["Coordinate Geometry"],
         difficulty: 2,
-        content: `若 $\\triangle ABC$ 為一直角三角形且 $\\angle ABC = 90^\\circ$，則下列何者正確？
+        content: `若 $\\Delta ABC$ 為一直角三角形且 $\\angle ABC = 90^\\circ$，則下列何者正確？
 <div class="q-mcms">
-I.\t$\\triangle ABC$ 的垂心在 $AC$ 上。<br>
-II.\t$\\triangle ABC$ 的形心位於 $\\triangle ABC$ 以內。<br>
-III.\t$\\triangle ABC$ 的內心位於 $\\triangle ABC$ 以外。
+I.\t$\\Delta ABC$ 的垂心在 $AC$ 上。
+II.\t$\\Delta ABC$ 的形心位於 $\\Delta ABC$ 以內。
+III.\t$\\Delta ABC$ 的內心位於 $\\Delta ABC$ 以外。
 </div>
 <div class="q-mcopt">
 A.\t只有 I<br>
@@ -700,7 +700,7 @@ D.\t只有 II 及 III
         domain: ["Data Handling"],
         topic: ["Probability"],
         difficulty: 3,
-        content: `某袋子內有 $2$ 個綠色杯、$8$ 個藍色杯及 $9$ 個紅色杯。若從該袋子內隨機同時抽出 $6$ 個杯，求抽出至少 $1$ 個藍色杯的概率。
+        content: `某袋子內有 $2$ 個綠色杯、$8$ 個藍色杯及 $9$ 個紅色杯。 若從該袋子內隨機同時抽出 $6$ 個杯，求抽出至少 $1$ 個藍色杯的概率。 
 <div class="q-mcopt">
 A.\t$\\dfrac{31}{57}$<br>
 B.\t$\\dfrac{44}{323}$<br>
@@ -716,12 +716,12 @@ D.\t$\\dfrac{968}{969}$
         domain: ["Data Handling"],
         topic: ["Probability"],
         difficulty: 2,
-        content: `某數學比賽共設三題。素珊答對第一題、答對第二題及答對第三題的概率分別為 $\\dfrac{1}{3}$、$\\dfrac{1}{5}$ 及 $\\dfrac{1}{7}$。素珊在該比賽中答對至多 $2$ 題的概率為
+        content: `某數學比賽共設三題。 素珊答對第一題、答對第二題及答對第三題的概率分別為 $\\dfrac{1}{3}$、$\\dfrac{1}{5}$ 及 $\\dfrac{1}{7}$。 素珊在該比賽中答對至多 $2$ 題的概率為
 <div class="q-mcopt">
-A.\t$\\dfrac{1}{105}$。<br>
-B.\t$\\dfrac{13}{105}$。<br>
-C.\t$\\dfrac{92}{105}$。<br>
-D.\t$\\dfrac{104}{105}$。
+A.\t$\\dfrac{1}{105}$。 <br>
+B.\t$\\dfrac{13}{105}$。 <br>
+C.\t$\\dfrac{92}{105}$。 <br>
+D.\t$\\dfrac{104}{105}$。 
 </div>`
     },
     {
@@ -732,12 +732,12 @@ D.\t$\\dfrac{104}{105}$。
         domain: ["Data Handling"],
         topic: ["Measures of Dispersion"],
         difficulty: 2,
-        content: `在某考試，考試得分的標準差為 $8$ 分。小麗的考試得分為 $69$ 分且她的標準分為 $0.5$。若偉明在該考試的標準分為 $-1.5$，則他的考試得分為
+        content: `在某考試，考試得分的標準差為 $8$ 分。 小麗的考試得分為 $69$ 分且她的標準分為 $0.5$。 若偉明在該考試的標準分為 $-1.5$，則他的考試得分為
 <div class="q-mcopt">
-A.\t$45$ 分。<br>
-B.\t$53$ 分。<br>
-C.\t$65$ 分。<br>
-D.\t$77$ 分。
+A.\t$45$ 分。 <br>
+B.\t$53$ 分。 <br>
+C.\t$65$ 分。 <br>
+D.\t$77$ 分。 
 </div>`
     },
     {
@@ -748,10 +748,10 @@ D.\t$77$ 分。
         domain: ["Data Handling"],
         topic: ["Measures of Central Tendency", "Measures of Dispersion"],
         difficulty: 2,
-        content: `某組數的平均值、分佈域及方差分別為 $m$、$r$ 及 $v$。將該組的每個數乘以 $6$ 後各加上 $5$ 而成新一組數。下列何者正確？
+        content: `某組數的平均值、分佈域及方差分別為 $m$、$r$ 及 $v$。 將該組的每個數乘以 $6$ 後各加上 $5$ 而成新一組數。 下列何者正確？
 <div class="q-mcms">
-I.\t新一組數的平均值為 $6m+5$。<br>
-II.\t新一組數的分佈域為 $6r+5$。<br>
+I.\t新一組數的平均值為 $6m+5$。
+II.\t新一組數的分佈域為 $6r+5$。
 III.\t新一組數的方差為 $6v+5$。
 </div>
 <div class="q-mcopt">
