199b065044edc28114c763b7e80776677a254b14
TH Wong <wongth@chusheklun.edu.hk>
2026-07-31 15:25:44 +0800

    Update 2025dsep2.js

--- 2025dsep2.js
+++ 2025dsep2.js
@@ -276,7 +276,7 @@ D.	$864\\pi$ cm$^3$。
         topic: ["Plane Geometry", "Areas and Volumes"],
         difficulty: 3,
         content: `圖中，$ABCD$ 為一平行四邊形。 設 $E$ 、 $F$ 及 $G$ 分別為 $AB$ 、 $AD$ 及 $CD$ 上的點使得 $BE = 3AE$ 、 $2DF = 3AF$ 及 $DG = 2CG$。 已知 $BC$ 的延線與 $EG$ 的延線相交於點 $H$。 若 $\\Delta CGH$ 的面積為 $16$ cm$^2$，則四邊形 $DFBG$ 的面積為
-<div class="q-mcopt"><img src="img/2025dsep2q17.jpg" class="q-img" alt="題目附圖" style="float: right; max-width: 60%; margin-left: 15px; margin-bottom: 5px;">
+<div class="q-mcopt" style="min-width: 500px;"><img src="img/2025dsep2q17.jpg" class="q-img" alt="題目附圖" style="float: right; max-width: 60%; margin-left: 15px; margin-bottom: 5px;">
 A.	$46$ cm$^2$。 <br>
 B.	$49$ cm$^2$。 <br>
 C.	$105$ cm$^2$。 <br>
