927708d18a208db9031cdcd8c9746dce914f1415
TH Wong <wongth@chusheklun.edu.hk>
2026-08-05 17:54:20 +0800

    Update 2025dsep2.js

--- 2025dsep2.js
+++ 2025dsep2.js
@@ -600,7 +600,7 @@ D.	$4$
         domain: ["Data Handling"],
         topic: ["Measures of Central Tendency", "Measures of Dispersion"],
         difficulty: 2,
-        content: `考慮以下數據：<div style="text-align: center;">$3x+2>$\\alpha$　　$\\beta$　　$-4$　　$-3$　　$1$　　$1$　　$1$　　$4$</div>
+        content: `考慮以下數據：<div style="text-align: center;">$\\alpha$　　$\\beta$　　$-4$　　$-3$　　$1$　　$1$　　$1$　　$4$</div>
 將以上數據的眾數及中位數分別記為 $s$ 及 $t$。 若以上數據的平均值及分佈域分別為 $0$ 及 $10$，則下列何者正確？
 <div class="q-mcms">
 I.	$s = 1$
