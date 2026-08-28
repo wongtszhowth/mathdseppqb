a170ac12cdf87dd7f5e49b6b4d6763f526b31697
TH Wong <wongth@chusheklun.edu.hk>
2026-08-28 19:40:07 +0800

    Update 2013dsep1.js

--- 2013dsep1.js
+++ 2013dsep1.js
@@ -11,8 +11,14 @@ questions.push(
         domain: ["Algebra"],
         topic: ["Laws of Indices"],
         difficulty: 1,
-        content: `化簡 $\\dfrac{x^{20}y^{13}}{(x^5y)^6}$，並以正指數表示答案。 <span class="marks">(3分)</span>`,
-        hint: `先展開分母指數，再應用指數定律。 `,
+        content: {
+            tc: `化簡 $\\dfrac{x^{20}y^{13}}{(x^5y)^6}$，並以正指數表示答案。<span class="marks">(3分)</span>`,
+            en: `Simplify $\\dfrac{x^{20}y^{13}}{(x^5y)^6}$ and express your answer with positive indices.<span class="marks">(3 marks)</span>`
+        },
+        hint: {
+            tc: `先展開分母指數，再應用指數定律。`,
+            en: `Expand the exponent in the denominator, then apply the index laws.`
+        },
         solution: `<div class="ms-container"><div class="ms-row"><div class="ms-content ms-from-l1">    $\\dfrac{x^{20}y^{13}}{(x^5y)^6}$</div><div class="ms-mark"></div></div><div class="ms-row"><div class="ms-content ms-from-l1">$=\\dfrac{x^{20}y^{13}}{x^{30}y^6}$</div><div class="ms-mark">1M</div></div><div class="ms-row"><div class="ms-content ms-from-l1">$=\\dfrac{y^{13-6}}{x^{30-20}}$</div><div class="ms-mark">1M</div></div><div class="ms-row"><div class="ms-content ms-from-l1">$=\\dfrac{y^7}{x^{10}}$</div><div class="ms-mark">1A</div></div></div>`
     },
     {
